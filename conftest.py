c154e69545998480de74d1fb938aaef6778dd97f
Manasa-kodeboyena <196140419+Manasa-kodeboyena@users.noreply.github.com>
2026-06-24 14:45:39 +0530

    fix: add headless Chrome options for CI/CD

--- conftest.py
+++ conftest.py
@@ -1,10 +1,16 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import pytest
+from selenium.webdriver.chrome.service import Service
+
 @pytest.fixture()
 def driver():
     options = Options()
+    options.add_argument("--headless")           
+    options.add_argument("--no-sandbox")         
+    options.add_argument("--disable-dev-shm-usage")  
     options.add_argument("--start-maximized")
+    
     driver = webdriver.Chrome(options=options)
     yield driver
-    driver.quit() 
+    driver.quit()
